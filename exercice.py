24ef953d611e9b346969c5eba3a7d8bfa219e595
byacine150 <58824990+byacine150@users.noreply.github.com>
2022-10-19 17:23:14 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,32 +3,33 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return ((voltage**2)/resistance)
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	if(v1[0]* v2[0] + v1[1]* v2[1] == 0):
+		return True
+	else: 
+		return False
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
-	for v in values:
-		pass # La variable v contient une valeur de la liste.
+	somme = 0
+	for element in values:
+		if element > 0:
+			somme = somme + element
+	return (somme/len(values))
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
-		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+	if value != 0:
+			twenties = int(value/20)
+			tens = int((value - (twenties * 20))/10)
+			fives = int((value - (twenties * 20)- (tens * 10))/5)
+			twos = int((value - (twenties * 20)- (tens * 10) - (fives * 5))/2)
+			ones = (value - (twenties * 20)- (tens * 10) - (fives * 5) - (twos*2))
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, twos, ones)
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
